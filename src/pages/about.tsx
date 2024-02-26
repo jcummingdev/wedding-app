54479d4cbe28c50d8d57f216cd70020bcd8507b7
sadxxface <120758081+sadxxface@users.noreply.github.com>
2024-02-25 16:07:33 -0800

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -15,13 +15,21 @@ export default function About(){
 
                 <p>A few months later I worked up the courage and sent him a friend request on Facebook and we quickly swapped emails so we could be in constant connection after that. It wasn&rsquo;t long before we were making plans to move into a glass house in a remote area of B.C. If you had told 13-year-old Shyanne their &lsquo;plans&rsquo; would eventually come true, she would have laughed at you.</p>
 
-                <p><Img src="/xmas.jpg" width={400} height={266} style={{float: 'none', padding: '10px 10px 10px 0px'}} alt="Christmas 2011"/>It took two years before I finally had the nerve to say yes to him. At 15, I think we both knew that this was it but what kind of easy-peasy life would that be?</p>
+                <div style={{width: '100%', height: '266px', position: 'relative'}}>
+                    <Img src={'/xmas.jpg'} fill={true} alt="Christmas 2011" style={{objectFit: 'contain', objectPosition: 'center'}}/>
+                </div>
+
+                <p>It took two years before I finally had the nerve to say yes to him. At 15, I think we both knew that this was it but what kind of easy-peasy life would that be?</p>
 
                 <p>After 13 crazy but magical years and two cats whom we can&rsquo;t get enough of, Jason asked if he could take me to Paris which I immediately followed with &ldquo;Are you proposing?&rdquo; Of course, he said no, but a day before we left I overheard him telling a mutual friend that he was in fact, proposing. <Img src="/halloween.jpg" width={225} height={225} style={{float: 'right', padding: '10px 0px 10px 10px'}} alt="This is hilarious"/>(I was literally two feet away from both of them). Despite knowing, the proposal was absolutely breathtaking. I mean, sunrise in front of the Eiffel Tower, are you kidding?</p>
 
                 <p>Jason and I grew up together. He was my best friend before he was anything else. I couldn&rsquo;t imagine a better happy ending. He is my light, my love &amp; everything in between. I cannot wait to say &lsquo;I do&rsquo;.</p>
-
-                <p><Img src="/happy.jpg" width={400} height={400} style={{float: 'none', padding: '10px 10px 10px 0px'}} alt="I don't even remember when this one was"/></p>
+                
+                <div style={{width: '100%', height: '400px', position: 'relative'}}>
+                    <Img src={'/happy.jpg'} fill={true} alt="Christmas 2011" style={{objectFit: 'contain', objectPosition: 'center'}}/>
+                
+                </div>
+                <p></p>
 
                 <p><strong>If you ask Jason:</strong> 15 years ago, I met Shyanne for the first time. Hard to believe that a random day in February of 2009 (couldn&rsquo;t tell you the exact date) turned out to be the most important day of my life. You can read the full story above but there was something about the not-so-6-foot-tall-and-bleach-blonde that just stuck with me. Maybe it was because of her goofiness, or maybe it was the <Link href='https://www.youtube.com/watch?v=7ETunGnK7bo' target='_blanks'>absolute bangers that she pumping out on YouTube</Link>. I&rsquo;m still not sure.</p>
 
